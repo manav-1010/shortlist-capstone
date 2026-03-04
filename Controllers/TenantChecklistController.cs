d883d886ba23b3c7d0a0c3270fbf3a4a971a1b25
Jenil Gohel <jgohel9157@conestogac.on.ca>
2026-03-03 20:30:46 -0500

    feat: add Tenant Checklist page (printable + scam checks)

---
+++ Controllers/TenantChecklistController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Shortlist.Web.Controllers
+{
+    [Authorize]
+    public class TenantChecklistController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
