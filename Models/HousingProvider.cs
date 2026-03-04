da1092f9ff18cf2f813176d30f99aa121a2a437c
Jenil Gohel <jgohel9157@conestogac.on.ca>
2026-03-03 20:23:27 -0500

    feat: update Housing Providers page

--- Models/HousingProvider.cs
+++ Models/HousingProvider.cs
@@ -3,8 +3,8 @@
     public class HousingProvider
     {
         public string Name { get; set; } = "";
+        public string Url { get; set; } = "";
         public string City { get; set; } = "";
-        public string WebsiteUrl { get; set; } = "";
         public string Tags { get; set; } = "";
         public string Notes { get; set; } = "";
     }
