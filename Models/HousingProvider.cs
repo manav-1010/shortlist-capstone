b3c9b91d02daaaa55faa713b40c3934ab64a4109
Jenil Gohel <jgohel9157@conestogac.on.ca>
2026-03-03 20:05:29 -0500

    feat: add HousingProvider model for rental company directory

---
+++ Models/HousingProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace Shortlist.Web.Models
+{
+    public class HousingProvider
+    {
+        public string Name { get; set; } = "";
+        public string City { get; set; } = "";
+        public string WebsiteUrl { get; set; } = "";
+        public string Tags { get; set; } = "";
+        public string Notes { get; set; } = "";
+    }
+}
