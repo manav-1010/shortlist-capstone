e862861692770b5d2869971d504adbd5386b2a87
Jenil Gohel <jgohel9157@conestogac.on.ca>
2026-03-04 08:22:58 -0500

    feat: add legal and copyright

---
+++ Views/Settings/Legal.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shortlist.Web.Views.Settings
+{
+    public class LegalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
