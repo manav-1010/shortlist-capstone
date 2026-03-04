97040e78ec3a4c3cc01b86a0e287c48ee10f5b48
Jenil Gohel <jgohel9157@conestogac.on.ca>
2026-03-03 20:37:49 -0500

    update TenantCheclist

---
+++ Views/TenantChecklist/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shortlist.Web.Views.TenantChecklist
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
