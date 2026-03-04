d5fa86f0af2ba271dd518395025f31c8a306c061
Jenil Gohel <jgohel9157@conestogac.on.ca>
2026-03-03 21:53:44 -0500

    feat: implement compare mode for listing comparison

---
+++ Controllers/CompareController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Shortlist.Web.Controllers
+{
+    public class CompareController : Controller
+    {
+        private const string SessionKey = "CompareList";
+
+        public IActionResult Index()
+        {
+            var json = HttpContext.Session.GetString(SessionKey);
+
+            if (string.IsNullOrEmpty(json))
+                return View(new List<int>());
+
+            var list = JsonSerializer.Deserialize<List<int>>(json);
+
+            return View(list);
+        }
+
+        [HttpPost]
+        public IActionResult Add(int id)
+        {
+            var json = HttpContext.Session.GetString(SessionKey);
+            var list = string.IsNullOrEmpty(json)
+                ? new List<int>()
+                : JsonSerializer.Deserialize<List<int>>(json);
+
+            if (!list.Contains(id) && list.Count < 3)
+                list.Add(id);
+
+            HttpContext.Session.SetString(SessionKey, JsonSerializer.Serialize(list));
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Remove(int id)
+        {
+            var json = HttpContext.Session.GetString(SessionKey);
+
+            if (string.IsNullOrEmpty(json))
+                return Ok();
+
+            var list = JsonSerializer.Deserialize<List<int>>(json);
+
+            list.Remove(id);
+
+            HttpContext.Session.SetString(SessionKey, JsonSerializer.Serialize(list));
+
+            return Ok();
+        }
+
+        public IActionResult Clear()
+        {
+            HttpContext.Session.Remove(SessionKey);
+            return RedirectToAction("Index");
+        }
+    }
+}
