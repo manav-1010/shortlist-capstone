2747086110e0617087a3048711c3232081269a0f
Jenil Gohel <jgohel9902@gmail.com>
2026-03-20 15:54:50 -0400

    add Api Model

---
+++ Models/RapidApiRealEstateOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace Shortlist.Web.Models
+{
+    public class RapidApiRealEstateOptions
+    {
+        public string ApiKey { get; set; } = string.Empty;
+        public string Host { get; set; } = string.Empty;
+        public string BaseUrl { get; set; } = string.Empty;
+        public string SearchPath { get; set; } = string.Empty;
+        public string DefaultCountry { get; set; } = "CA";
+    }
+}
