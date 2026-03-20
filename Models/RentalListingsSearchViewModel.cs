fe5420543f50d84228fcc5435eef0808bbbd988a
Jenil Gohel <jgohel9902@gmail.com>
2026-03-20 15:50:12 -0400

    add RentalListingsSearchViewModel

---
+++ Models/RentalListingsSearchViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Shortlist.Web.Models
+{
+    public class RentalListingsSearchViewModel
+    {
+        public string Query { get; set; } = string.Empty;
+        public string City { get; set; } = "Kitchener";
+        public string ProvinceOrState { get; set; } = "ON";
+        public int Page { get; set; } = 1;
+        public int MaxPrice { get; set; } = 0;
+        public int MinBeds { get; set; } = 0;
+        public string Sort { get; set; } = "relevance";
+    }
+}
