2410acd4310be565e66678d44ec70c1816d64717
Jenil Gohel <jgohel9902@gmail.com>
2026-03-20 15:53:06 -0400

    add RentalListingsPageViewModel

---
+++ Models/RentalListingsPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Shortlist.Web.Models
+{
+    public class RentalListingsPageViewModel
+    {
+        public RentalListingsSearchViewModel Search { get; set; } = new();
+        public List<RentalListingCard> Listings { get; set; } = new();
+
+        public bool HasResults => Listings.Count > 0;
+        public string StatusMessage { get; set; } = string.Empty;
+        public string ErrorMessage { get; set; } = string.Empty;
+    }
+}
