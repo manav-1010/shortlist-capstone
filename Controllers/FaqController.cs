0cf750c7601af1f807d117e10aba1314281a43d5
Jenil Gohel <jgohel9157@conestogac.on.ca>
2026-04-08 11:26:28 -0400

    chore: add Assistant and FAQ Controller

---
+++ Controllers/FaqController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shortlist.Web.Controllers
+{
+    public class FaqController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
