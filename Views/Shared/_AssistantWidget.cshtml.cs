de2042d129af8bdf8a4e7513ed3b340b8c53c362
Jenil Gohel <jgohel9157@conestogac.on.ca>
2026-04-08 11:56:36 -0400

    chore: update database and finalize AIAssistant alongwith FAQs

---
+++ Views/Shared/_AssistantWidget.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shortlist.Web.Views.Shared
+{
+    public class _AssistantWidgetModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
